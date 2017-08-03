fcb45c976617096db1335119a5dc063773fc4525
kwilsonO <kylewilson52@gmail.com>
2017-08-03 13:44:14 -0400

    got full locator working, now just need to properly display the data and such

--- locate-stores.php
+++ locate-stores.php
@@ -4,9 +4,20 @@
 <?php
 chdir(dirname(__FILE__));
 require_once 'secrets/zip-code-api-key.php';
+$servername = "localhost";
+$username = "testuser";
+$password = "password";
+$dbname = "testdb";
 $zip_code = $_POST["zipcode"];
 $radius= $_POST["radius"];
 $apiurl = sprintf("https://www.zipcodeapi.com/rest/%s/radius.csv/%s/%s/miles?minimal", $api_key, $zip_code, $radius);
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 ?>
 
 Current Zip Code: <?php echo $zip_code; ?><br>
@@ -16,7 +27,24 @@ URL: <?php echo $apiurl; ?><br>
 
 <?php
 $result=CallApi("GET", $apiurl);
-echo $result;
+echo $result . "<br>";
+//replace the zip_code text from results
+$result = str_replace("zip_code","",$result);
+$result = preg_replace('#\s+#',', ',trim($result));
+echo $result . "<br>";
+$sql= sprintf("SELECT id, name, address, zip_code, phone  FROM CLIENT_DATA WHERE zip_code IN (%s)",$result);
+echo $sql . "<br>";
+$queryresult = $conn->query($sql);
+
+if ($queryresult->num_rows > 0) {
+    while($row = $queryresult->fetch_assoc()) {
+	echo $row;
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
 
 
