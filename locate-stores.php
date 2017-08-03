c4d877b755ac5496b1a7b5f24ccfc37c5f72cb80
kwilsonO <kylewilson52@gmail.com>
2017-08-03 12:03:37 -0400

    added quick script to update root folder of apache2 server, and changed the GET to POST to get the form values

--- locate-stores.php
+++ locate-stores.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Current Zip Code: <?php echo $_GET["zipcode"]; ?><br>
-Radius Selected: <?php echo $_GET["radius"]; ?><br>
+Current Zip Code: <?php echo $_POST["zipcode"]; ?><br>
+Radius Selected: <?php echo $_POST["radius"]; ?><br>
 
 </body>
 <html>
